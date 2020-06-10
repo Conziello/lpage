7b57b5ce960833f1419b84c395ece669d6392710
Conziello <conziello@gmail.com>
2020-06-09 23:18:29 -0700

    commit

---
+++ contact-us.php
@@ -0,0 +1,324 @@
+<?php 
+include 'db_fns.php';
+?>
+<!DOCTYPE html>
+<html>
+	
+<!-- Mirrored from preview.oklerthemes.com/porto/8.0.0/contact-us.html by HTTrack Website Copier/3.x [XR&CO'2014], Tue, 09 Jun 2020 09:40:00 GMT -->
+<head>
+
+		<!-- Basic -->
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">	
+
+		<title>Contact Us | Porto - Responsive HTML5 Template</title>	
+
+		<meta name="keywords" content="HTML5 Template" />
+		<meta name="description" content="Porto - Responsive HTML5 Template">
+		<meta name="author" content="okler.net">
+
+		<!-- Favicon -->
+		<link rel="shortcut icon" href="img/favicon.ico" type="image/x-icon" />
+		<link rel="apple-touch-icon" href="img/apple-touch-icon.png">
+
+		<!-- Mobile Metas -->
+		<meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1.0, shrink-to-fit=no">
+
+		<!-- Web Fonts  -->
+		<link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700,800%7CShadows+Into+Light" rel="stylesheet" type="text/css">
+
+		<!-- Vendor CSS -->
+		<link rel="stylesheet" href="vendor/bootstrap/css/bootstrap.min.css">		<link rel="stylesheet" href="vendor/fontawesome-free/css/all.min.css">		<link rel="stylesheet" href="vendor/animate/animate.min.css">		<link rel="stylesheet" href="vendor/simple-line-icons/css/simple-line-icons.min.css">		<link rel="stylesheet" href="vendor/owl.carousel/assets/owl.carousel.min.css">		<link rel="stylesheet" href="vendor/owl.carousel/assets/owl.theme.default.min.css">		<link rel="stylesheet" href="vendor/magnific-popup/magnific-popup.min.css">
+
+		<!-- Theme CSS -->
+		<link rel="stylesheet" href="css/theme.css">
+		<link rel="stylesheet" href="css/theme-elements.css">
+		<link rel="stylesheet" href="css/theme-blog.css">
+		<link rel="stylesheet" href="css/theme-shop.css">
+		
+		<!-- Demo CSS -->
+
+
+		<!-- Skin CSS -->
+		<link rel="stylesheet" href="css/skins/default.css">		<script src="master/style-switcher/style.switcher.localstorage.js"></script> 
+
+		<!-- Theme Custom CSS -->
+		<link rel="stylesheet" href="css/custom.css">
+
+		<!-- Head Libs -->
+		<script src="vendor/modernizr/modernizr.min.js"></script>
+
+	</head>
+	<body>
+
+		<div class="body">
+			<header id="header" class="header-effect-shrink" data-plugin-options="{'stickyEnabled': true, 'stickyEffect': 'shrink', 'stickyEnableOnBoxed': true, 'stickyEnableOnMobile': true, 'stickyChangeLogo': true, 'stickyStartAt': 30, 'stickyHeaderContainerHeight': 70}">
+				<div class="header-body">
+					<div class="header-container container">
+						<div class="header-row">
+							<div class="header-column">
+								<div class="header-row">
+									<div class="header-logo">
+										<a href="index-2.html">
+											<img alt="Porto" width="100" height="48" data-sticky-width="82" data-sticky-height="40" src="img/logo.png">
+										</a>
+									</div>
+								</div>
+							</div>
+							<div class="header-column justify-content-end">
+								<div class="header-row">
+									<div class="header-nav header-nav-line header-nav-top-line header-nav-top-line-with-border order-2 order-lg-1">
+										<div class="header-nav-main header-nav-main-square header-nav-main-effect-2 header-nav-main-sub-effect-1">
+											<nav class="collapse">
+												<ul class="nav nav-pills" id="mainNav">
+													
+												</ul>
+											</nav>
+										</div>
+										<button class="btn header-btn-collapse-nav" data-toggle="collapse" data-target=".header-nav-main nav">
+											<i class="fas fa-bars"></i>
+										</button>
+									</div>
+									<div class="header-nav-features header-nav-features-no-border header-nav-features-lg-show-border order-1 order-lg-2">										<div class="header-nav-feature header-nav-features-search d-inline-flex">											<a href="#" class="header-nav-features-toggle" data-focus="headerSearch"><i class="fas fa-search header-nav-top-icon"></i></a>											<div class="header-nav-features-dropdown" id="headerTopSearchDropdown">												<form role="search" action="https://preview.oklerthemes.com/porto/8.0.0/page-search-results.html" method="get">													<div class="simple-search input-group">														<input class="form-control text-1" id="headerSearch" name="q" type="search" value="" placeholder="Search...">														<span class="input-group-append">															<button class="btn" type="submit">																<i class="fa fa-search header-nav-top-icon"></i>															</button>														</span>													</div>												</form>											</div>										</div>									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</header>
+
+			<div role="main" class="main">
+
+				<!-- Google Maps - Go to the bottom of the page to change settings and map location.
+				<div id="googlemaps" class="google-map mt-0" style="height: 500px;">
+					<div id="map"><iframe src="https://www.google.com/maps/embed?pb=!1m23!1m12!1m3!1d127653.29881780625!2d37.00266688641212!3d-1.0364170257794505!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!4m8!3e0!4m0!4m5!1s0x182f4e8ff903c717%3A0x8012052d7cbd94e4!2sNaivas%20Supermarket%20-%20Thika%20Town%2C%20Workshop%20Rd%2C%20Thika!3m2!1d-1.0364178!2d37.0727075!5e0!3m2!1sen!2ske!4v1567366265212!5m2!1sen!2ske" width="1450" height="281" frameborder="0" style="border:0;" allowfullscreen=""></iframe></div>
+				</div> -->
+
+				<div class="container">
+
+					<div class="row py-4">
+						<div class="col-lg-6">
+
+							<div class="overflow-hidden mb-1">
+								<h2 class="font-weight-normal text-7 mt-2 mb-0 appear-animation" data-appear-animation="maskUp" data-appear-animation-delay="200"><strong class="font-weight-extra-bold">Contact</strong> Us</h2>
+							</div>
+							<div class="overflow-hidden mb-4 pb-3">
+								<p class="mb-0 appear-animation" data-appear-animation="maskUp" data-appear-animation-delay="400">Feel free to ask for details, don't save any questions!</p>
+							</div>
+
+							<form class="contact-form" action="process.php" method="POST">
+								<div class="contact-form-success alert alert-success d-none mt-4">
+									<strong>Success!</strong> Your message has been sent to us.
+								</div>
+							
+								<div class="contact-form-error alert alert-danger d-none mt-4">
+									<strong>Error!</strong> There was an error sending your message.
+									<span class="mail-error-message text-1 d-block"></span>
+								</div>
+								
+								<div class="form-row">
+									<div class="form-group col-lg-6">
+										<label class="required font-weight-bold text-dark text-2">Your Name</label>
+										<input type="text" value="" data-msg-required="Please enter your name or your business name." maxlength="100" class="form-control" name="name" required>
+									</div>
+									<div class="form-group col-lg-6">
+										<label class="required font-weight-bold text-dark text-2">Your Email Address</label>
+										<input type="email" value="" data-msg-required="Please enter your email address." data-msg-email="Please enter a valid email address." maxlength="100" class="form-control" name="email" required>
+									</div>
+								</div>
+								<div class="form-row">
+									<div class="form-group col">
+										<label class="font-weight-bold text-dark text-2">Your Location</label>
+										<input type="text" value="" data-msg-required="Please enter your location." maxlength="100" class="form-control" name="location" required>
+									</div>
+								</div>
+								<div class="form-row">
+									<div class="form-group col">
+										<label class="font-weight-bold text-dark text-2">Your line of business</label>
+										<input type="text" value="" data-msg-required="Please enter your line of business" maxlength="100" class="form-control" name="business" >
+									</div>
+								</div>
+								<div class="form-row">
+									<div class="form-group col">
+										<label class="required font-weight-bold text-dark text-2">Message</label>
+										<textarea maxlength="5000" data-msg-required="Please enter your message." rows="8" class="form-control" name="description" required></textarea>
+									</div>
+								</div>
+								<div class="form-row">
+									<div class="form-group col">
+										<label class="required font-weight-bold text-dark text-2">Phone Number</label>
+										<input type="number" value="" data-msg-required="Please enter your phone number." rows="8" class="form-control" name="response" required>
+									</div>
+								</div>
+								<!-- <div class="form-row">
+									<div class="form-group col">
+										<label class="required font-weight-bold text-dark text-2">Date</label>
+										<input type="date" value="" data-msg-required="Please enter your date." rows="8" class="form-control" name="response" >
+									</div>
+								</div> -->
+								<div class="form-row">
+									<div class="form-group col">
+										<input type="submit" name="submit" value="Send Message" class="btn btn-primary btn-modern" >
+									</div>
+								</div>
+							</form>
+
+						</div>
+						<div class="col-lg-6">
+
+							<div class="appear-animation" data-appear-animation="fadeIn" data-appear-animation-delay="800">
+								<h4 class="mt-2 mb-1">Our <strong>Office</strong></h4>
+								<ul class="list list-icons list-icons-style-2 mt-2">
+									<li><i class="fas fa-map-marker-alt top-6"></i> <strong class="text-dark">Address:</strong> Thika Business Centre, Thika, Kenya</li>
+									<li><i class="fas fa-phone top-6"></i> <strong class="text-dark">Phone:</strong> (254) 741210049</li>
+									<li><i class="fas fa-envelope top-6"></i> <strong class="text-dark">Email:</strong> <a href="mailto:mail@example.com"> info@qet.co.ke</a></li>
+								</ul>
+							</div>
+
+							<div class="appear-animation" data-appear-animation="fadeIn" data-appear-animation-delay="950">
+								<h4 class="pt-5">Business <strong>Hours</strong></h4>
+								<ul class="list list-icons list-dark mt-2">
+									<li><i class="far fa-clock top-6"></i> Monday - Friday - 9am to 5pm</li>
+									<li><i class="far fa-clock top-6"></i> Saturday - Closed</li>
+									<li><i class="far fa-clock top-6"></i> Sunday - Closed</li>
+								</ul>
+							</div>
+
+							<h4 class="pt-5">Get in <strong>Touch</strong></h4>
+							<!-- <p class="lead mb-0 text-4">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur eget leo at velit imperdiet varius. In eu ipsum vitae velit congue iaculis vitae at risus. Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p> -->
+
+						</div>
+
+					</div>
+
+				</div>
+
+			</div>
+
+			<!-- <section class="call-to-action call-to-action-default with-button-arrow content-align-center call-to-action-in-footer">
+				<div class="container">
+					<div class="row">
+						<div class="col-md-9 col-lg-9">
+							<div class="call-to-action-content">
+								<h2 class="font-weight-normal text-6 mb-0">Porto is <strong class="font-weight-extra-bold">everything</strong> you need to create an <strong class="font-weight-extra-bold">awesome</strong> website!</h2>
+								<p class="mb-0">The <strong class="font-weight-extra-bold">Best</strong> HTML Site Template on ThemeForest</p>
+							</div>
+						</div>
+						<div class="col-md-3 col-lg-3">
+							<div class="call-to-action-btn">
+								<a href="http://themeforest.net/item/porto-responsive-html5-template/4106987" target="_blank" class="btn btn-dark btn-lg text-3 font-weight-semibold px-4 py-3">Get Started Now</a><span class="arrow hlb d-none d-md-block" data-appear-animation="rotateInUpLeft" style="top: -40px; left: 70%;"></span>
+							</div>
+						</div>
+					</div>
+				</div>
+			</section> -->
+
+			<footer id="footer">			
+					<div class="container">		
+									<div class="footer-ribbon">		
+														<span>Get in Touch</span>	
+																	</div>			
+																			 <div class="row py-5 my-4">					<!--	<div class="col-md-12 col-lg-4 mb-4 mb-lg-0">						 -->
+				
+				
+				
+				<!-- <h5 class="text-3 mb-3">NEWSLETTER</h5>				
+							<p class="pr-1">Keep up on our always evolving product features and technology. Enter your e-mail address and subscribe to our newsletter.</p>	
+													<div class="alert alert-success d-none" id="newsletterSuccess">								<strong>Success!</strong> You've been added to our email list.						
+														</div>						
+															<div class="alert alert-danger d-none" id="newsletterError"></div>
+																						<form id="newsletterForm" action="https://preview.oklerthemes.com/porto/8.0.0/php/newsletter-subscribe.php" method="POST" class="mr-4 mb-3 mb-md-0">								<div class="input-group input-group-rounded">									<input class="form-control form-control-sm bg-light" placeholder="Email Address" name="newsletterEmail" id="newsletterEmail" type="text">									<span class="input-group-append">										<button class="btn btn-light text-color-dark" type="submit"><strong>GO!</strong></button>									</span>								</div>							</form>						</div>						<div class="col-md-6 col-lg-3 mb-4 mb-lg-0">							<h5 class="text-3 mb-3">LATEST TWEETS</h5>							<div id="tweet" class="twitter" data-plugin-tweets data-plugin-options="{'username': 'oklerthemes', 'count': 2}">								<p>Please wait...</p>							</div>						</div>					 -->
+				
+				
+				
+				<div class="col-md-6 col-lg-3 mb-4 mb-md-0">							<div class="contact-details">								<h5 class="text-3 mb-3">CONTACT US</h5>								<ul class="list list-icons list-icons-lg">									<li class="mb-1"><i class="far fa-dot-circle text-color-primary"></i><p class="m-0">Thika Business Centre 5th floor, Thika Kenya</p></li>									<li class="mb-1"><i class="fab fa-whatsapp text-color-primary"></i><p class="m-0"><a href="tel:8001234567">0741210049</a></p></li>									<li class="mb-1"><i class="far fa-envelope text-color-primary"></i><p class="m-0"><a href="mailto:mail@example.com">info@qet.co.ke</a></p></li>								</ul>							</div>						</div>						<div class="col-md-6 col-lg-2">							<h5 class="text-3 mb-3">FOLLOW US</h5>							<ul class="social-icons">								<li class="social-icons-facebook"><a href="http://www.facebook.com/" target="_blank" title="Facebook"><i class="fab fa-facebook-f"></i></a></li>								<li class="social-icons-twitter"><a href="http://www.twitter.com/" target="_blank" title="Twitter"><i class="fab fa-twitter"></i></a></li>								<li class="social-icons-linkedin"><a href="http://www.linkedin.com/" target="_blank" title="Linkedin"><i class="fab fa-linkedin-in"></i></a></li>							</ul>						</div>					</div>				</div>			
+					<div class="footer-copyright">					
+						<div class="container py-2">					
+								<div class="row py-4">				
+												<div class="col-lg-1 d-flex align-items-center justify-content-center justify-content-lg-start mb-2 mb-lg-0">								<a href="index-2.html" class="logo pr-0 pr-lg-3">									<img alt="Porto Website Template" src="img/logo.png" class="opacity-5" height="33">								</a>							</div>							
+												<div class="col-lg-7 d-flex align-items-center justify-content-center justify-content-lg-start mb-4 mb-lg-0">								<p>© Copyright 2020. All Rights Reserved.</p>							</div>							<div class="col-lg-4 d-flex align-items-center justify-content-center justify-content-lg-end">								<nav id="sub-menu">									<ul>																													<li><i class="fas fa-angle-right"></i><a href="http://qet.co.ke" class="ml-1 text-decoration-none"> QET </a></li>									</ul>								</nav>							</div>						</div>					</div>				</div>			</footer>
+		</div>
+
+		<!-- Vendor -->
+		<script src="vendor/jquery/jquery.min.js"></script>		<script src="vendor/jquery.appear/jquery.appear.min.js"></script>		<script src="vendor/jquery.easing/jquery.easing.min.js"></script>		<script src="vendor/jquery.cookie/jquery.cookie.min.js"></script>		<script src="master/style-switcher/style.switcher.js" id="styleSwitcherScript" data-base-path="" data-skin-src=""></script>		<script src="vendor/popper/umd/popper.min.js"></script>		<script src="vendor/bootstrap/js/bootstrap.min.js"></script>		<script src="vendor/common/common.min.js"></script>		<script src="vendor/jquery.validation/jquery.validate.min.js"></script>		<script src="vendor/jquery.easy-pie-chart/jquery.easypiechart.min.js"></script>		<script src="vendor/jquery.gmap/jquery.gmap.min.js"></script>		<script src="vendor/jquery.lazyload/jquery.lazyload.min.js"></script>		<script src="vendor/isotope/jquery.isotope.min.js"></script>		<script src="vendor/owl.carousel/owl.carousel.min.js"></script>		<script src="vendor/magnific-popup/jquery.magnific-popup.min.js"></script>		<script src="vendor/vide/jquery.vide.min.js"></script>		<script src="vendor/vivus/vivus.min.js"></script>
+		
+		<!-- Theme Base, Components and Settings -->
+		<script src="js/theme.js"></script>
+
+		<!-- Current Page Vendor and Views -->
+		<!-- <script src="js/views/view.contact.js"></script> -->
+
+		
+		<!-- Theme Initialization Files -->
+		<script src="js/theme.init.js"></script>
+
+		<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAhpYHdYRY2U6V_VfyyNtkPHhywLjDkhfg"></script>
+		<script>
+
+			/*
+			Map Settings
+
+				Find the Latitude and Longitude of your address:
+					- https://www.latlong.net/
+					- http://www.findlatitudeandlongitude.com/find-address-from-latitude-and-longitude/
+
+			*/
+
+			// Map Markers
+			var mapMarkers = [{
+				address: "New York, NY 10017",
+				html: "<strong>New York Office</strong><br>New York, NY 10017",
+				icon: {
+					image: "img/pin.png",
+					iconsize: [26, 46],
+					iconanchor: [12, 46]
+				},
+				popup: true
+			}];
+
+			// Map Initial Location
+			var initLatitude = 40.75198;
+			var initLongitude = -73.96978;
+
+			// Map Extended Settings
+			var mapSettings = {
+				controls: {
+					draggable: (($.browser.mobile) ? false : true),
+					panControl: true,
+					zoomControl: true,
+					mapTypeControl: true,
+					scaleControl: true,
+					streetViewControl: true,
+					overviewMapControl: true
+				},
+				scrollwheel: false,
+				markers: mapMarkers,
+				latitude: initLatitude,
+				longitude: initLongitude,
+				zoom: 11
+			};
+
+			var map = $('#googlemaps').gMap(mapSettings);
+
+			// Map text-center At
+			var mapCenterAt = function(options, e) {
+				e.preventDefault();
+				$('#googlemaps').gMap("centerAt", options);
+			}
+
+		</script>
+
+		<script>
+			(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+			(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+			m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+			})(window,document,'script','../../../www.google-analytics.com/analytics.js','ga');
+		
+			ga('create', 'UA-42715764-5', 'auto');
+			ga('send', 'pageview');
+		</script>
+
+	</body>
+
+<!-- Mirrored from preview.oklerthemes.com/porto/8.0.0/contact-us.html by HTTrack Website Copier/3.x [XR&CO'2014], Tue, 09 Jun 2020 09:40:00 GMT -->
+</html>
